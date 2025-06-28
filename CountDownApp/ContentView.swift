9a18b7a31c34f10bf41b06081e0c096c83a31c68
Aziz Bibitov <azizbibitov@Azizs-MacBook-Air.local>
2025-06-28 17:00:09 +0500

    project created

---
+++ CountDownApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDownApp
+//
+//  Created by Aziz Bibitov on 28/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDownApp/CountDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownAppApp.swift
+//  CountDownApp
+//
+//  Created by Aziz Bibitov on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
